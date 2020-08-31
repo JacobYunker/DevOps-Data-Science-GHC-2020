5a29a572cfc8391a5a6353eae601f7dadf51caa0
JacobYunker <jacobyunker@gmail.com>
2020-08-31 18:08:51 -0500

    Update app.R

--- app/app.R
+++ app/app.R
@@ -5,7 +5,7 @@ housing<-read.csv("BostonHousing.csv", header=TRUE)
 
 ui<-fluidPage(
 
-    titlePanel("Boston Housing Data"),
+    titlePanel("Boston Housing Data Visualization"),
 
     sidebarLayout(
         sidebarPanel(
